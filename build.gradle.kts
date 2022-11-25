7e43734c26b973c2fcd0fdafdfae6be0f56ef231
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-11-24 19:15:13 -0500

    ⚡️Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.0.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.graalvm.buildtools.native") version "0.9.18"
+}
+
+group = "org.rrajesh1979"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/rrajesh1979/javaspringmongorestref/JavaSpringMongoRestRefApplication.java
@@ -0,0 +1,13 @@
+package org.rrajesh1979.javaspringmongorestref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMongoRestRefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringMongoRestRefApplication.class, args);
+    }
+
+}
